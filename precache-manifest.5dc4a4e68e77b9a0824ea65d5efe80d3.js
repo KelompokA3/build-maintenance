1743c1ef8f876045a57471ef529abe2b697ec58e
rayhantif <rayhan.zharfan.tif18@polban.ac.id>
2020-03-17 13:01:07 +0700

    first commit

---
+++ precache-manifest.5dc4a4e68e77b9a0824ea65d5efe80d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d3be2034a4fc6a58649",
+    "url": "/static/css/main.2c47ff56.chunk.css"
+  },
+  {
+    "revision": "1d3be2034a4fc6a58649",
+    "url": "/static/js/main.0b50b047.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bcb93014acac18fd31ff",
+    "url": "/static/js/2.bd81f336.chunk.js"
+  },
+  {
+    "revision": "e250a0b5dac43c9692d721e9aa5c7f4b",
+    "url": "/static/media/gear.e250a0b5.png"
+  },
+  {
+    "revision": "0da914770b48b1bf3f2edd9d8af9b6ca",
+    "url": "/static/media/3526768.0da91477.jpg"
+  },
+  {
+    "revision": "d4e90b4d3034b7c3dc0239934707714d",
+    "url": "/static/media/check-mark.d4e90b4d.svg"
+  },
+  {
+    "revision": "906266bc45dc4e7a1423a4455c72928c",
+    "url": "/static/media/instaicon.906266bc.svg"
+  },
+  {
+    "revision": "3137fd491507f10d438b2249da031569",
+    "url": "/static/media/twittericon.3137fd49.svg"
+  },
+  {
+    "revision": "3c9ad1a3b6ad3ac5de6bd62a5e1fe995",
+    "url": "/static/media/fbookicon.3c9ad1a3.svg"
+  },
+  {
+    "revision": "a274ef32a2b0846d2afa5a56d2c59aa3",
+    "url": "/static/media/youtubeicon.a274ef32.svg"
+  },
+  {
+    "revision": "c261f72b7fe0963bdeabe0dc3a770cb8",
+    "url": "/index.html"
+  }
+];
